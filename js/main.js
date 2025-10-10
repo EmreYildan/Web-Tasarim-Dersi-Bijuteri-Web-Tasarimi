0ed43fa97a65c5e89293a90c063ebe1efd2971c6
EmreYildan <emreyildan@gmail.com>
2025-10-10 20:13:09 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,86 @@
+function mobileCheck(){
+	var winWidth=$(window).width();
+	if (winWidth<=768) {
+		$("#sidebar").after($("#body .pagination:first"))
+	} else {
+		$(".products-wrap").before($("#body .pagination:first"))
+	}
+}
+
+$(document).ready(function() {
+	$("input[type=checkbox]").crfi();
+	$("select").crfs();
+	$("#slider ul").bxSlider({
+		controls: true,
+		auto: true,
+		mode: 'fade',
+		preventDefaultSwipeX: false,
+		pause: 5000,
+		speed: 800
+	});
+	
+	// Add click functionality to slider items
+	$("#slider li").click(function() {
+		window.location.href = 'product.html';
+	});
+	$(".last-products .products").bxSlider({
+		pager: false,
+		minSlides: 1,
+		maxSlides: 5,
+		slideWidth: 235,
+		slideMargin: 0
+	});
+	$(".tabs .nav a").click(function() {
+		var container = $(this).parentsUntil(".tabs").parent();
+		if (!$(this).parent().hasClass("active")) {
+			container.find(".nav .active").removeClass("active")
+			$(this).parent().addClass("active")
+			container.find(".tab-content").hide()
+			$($(this).attr("href")).show();
+		};
+		return false;
+	});
+	$("#price-range").slider({
+		range: true,
+		min: 0,
+		max: 5000,
+		values: [ 500, 3500 ],
+		slide: function( event, ui ) {
+			$(".ui-slider-handle:first").html("<span>$" + ui.values[ 0 ] + "</span>");
+			$(".ui-slider-handle:last").html("<span>$" + ui.values[ 1 ] + "</span>");
+		}
+	});
+	$(".ui-slider-handle:first").html("<span>$" + $( "#price-range" ).slider( "values", 0 ) + "</span>");
+	$(".ui-slider-handle:last").html("<span>$" + $( "#price-range" ).slider( "values", 1 ) + "</span>");
+	$("#menu .trigger").click(function() {
+		$(this).toggleClass("active").next().toggleClass("active")
+	});
+
+	// Modal functionality for announcements
+	$(".announcement-item, .announcement-full").click(function() {
+		var title = $(this).find("h3, h2").first().text();
+		var content = $(this).data("content");
+		$("#modal-title").text(title);
+		$("#modal-content").html(content);
+		$("#announcement-modal").fadeIn();
+	});
+	
+	$(".modal-close, .modal-overlay").click(function() {
+		$("#announcement-modal, #success-modal").fadeOut();
+	});
+	
+	// Dropdown menu functionality
+	$(".menu-item.has-submenu").hover(
+		function() {
+			$(this).find(".submenu").slideDown(200);
+		},
+		function() {
+			$(this).find(".submenu").slideUp(200);
+		}
+	);
+
+	mobileCheck();
+	$(window).resize(function() {
+		mobileCheck();
+	});
+});
